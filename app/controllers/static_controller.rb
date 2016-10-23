e7c4cad1f60031fbca82af2f644acabd2cc96529
Chris Scott <cyberstrike@gmail.com>
2016-10-23 05:53:49 -0400

    added category routes

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,6 +1,8 @@
 class StaticController < ApplicationController
   def index
   end
+
   def show
+    ap params[:category]
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   root 'static#index'
   get 'static/index'
-
+  get 'static/:category' => 'static#show', as: :static
 
   resources :update_items
-  get 'static/sport'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
