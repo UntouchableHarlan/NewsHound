cb8b15633ef42722796c4eba7d028131201e2264
Philco Michel <phil9231@gmail.com>
2016-10-22 23:15:02 -0400

    home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   get 'static/index'
+  
 
   resources :update_items
+  get 'static/sport'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+	
