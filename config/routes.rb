51af23b29791b8c24920de1ba6fd362a5e1c5234
Chris Scott <cyberstrike@gmail.com>
2016-10-23 01:47:15 -0400

    lots of styles....

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+  root 'static#index'
   get 'static/index'
-  
+
 
   resources :update_items
   get 'static/sport'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-	
